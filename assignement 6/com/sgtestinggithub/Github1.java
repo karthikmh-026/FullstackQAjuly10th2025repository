749413ab2851c011902c00580de36a781d324211
karthikmh-026 <karthikhiremath026@gmail.com>
2025-08-13 14:41:40 +0530

    secondcommit

---
+++ assignement 6/com/sgtestinggithub/Github1.java
@@ -0,0 +1,11 @@
+package com.sgtestinggithub;
+
+public class Github1
+{
+    public static void main(String[] args) {
+        for (int i=20;i<=50;i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
